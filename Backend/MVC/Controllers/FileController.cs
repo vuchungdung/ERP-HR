f30edc895fe51ec3ab55e278babc43187ec3805e
ADMIN <ADMIN@DESKTOP-6HF17VS>
2020-12-02 01:41:14 +0700

    update form apply job for project MVC

---
+++ Backend/MVC/Controllers/FileController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Services.Common.ViewModel;
+
+namespace MVC.Controllers
+{
+    public class FileController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult UpLoad()
+        {
+            FileCvViewModel model = new FileCvViewModel();
+            var file = Request.Form.Files[0];
+            var folderName = Path.Combine(@"D:\ERP-Recruiting\Backend\APIGateway\wwwroot/cadidate-cv");
+            var pathToSave = Path.Combine(Directory.GetCurrentDirectory(), folderName);
+            if (file.Length > 0)
+            {
+                var fileName = ContentDispositionHeaderValue.Parse(file.ContentDisposition).FileName.Trim('"');
+                var fullPath = Path.Combine(pathToSave, fileName);
+                var dbPath = Path.Combine(folderName, fileName);
+                var fileSize = file.Length / 1024;
+                var fileType = Path.GetExtension(fileName);
+                
+                model.FileName = fileName;
+                model.FilePath = fullPath;
+                model.FileSize = Convert.ToInt32(fileSize);
+                model.FileType = fileType;
+                model.DbPath = dbPath;
+                using (var stream = new FileStream(fullPath, FileMode.Create))
+                {
+                    file.CopyTo(stream);
+                }
+            }
+            return Json(model);
+        }
+    }
+}
--- Backend/MVC/wwwroot/app/controller/cadidateController.js
+++ Backend/MVC/wwwroot/app/controller/cadidateController.js
@@ -2,9 +2,9 @@
 
     app.controller('cadidateController', cadidateController);
 
-    cadidateController.$inject = ['$scope','ajaxService'];
+    cadidateController.$inject = ['$scope', 'ajaxService','$http'];
 
-    function cadidateController($scope, ajaxService) {
+    function cadidateController($scope, ajaxService, $http) {
 
         $scope.user = {
             Username: "",
@@ -48,6 +48,45 @@
                 }
             })
         }
+
+
+
+        $scope.img = '';
+        $scope.pdf = '';
+        $scope.postCadidate = function () {
+            console.log($scope.cadidate.img);
+            console.log($scope.cadidate.pdf);
+        }
+
+        $scope.remove = function () {
+            $scope.img = '';
+            $scope.pdf = '';
+            alert("Gì cơ")
+        }
+
+        $scope.getFile = function (element) {
+            var file = element.files[0];
+            if (typeof (file) == 'undefined') {
+                alert("Gì đấy")
+            }
+            else {
+                var formData = new FormData();
+                formData.append('file', file);
+                $http({
+                    url: '/File/UpLoad',
+                    method: 'POST',
+                    data: formData,
+                    headers: { 'Content-Type': undefined }
+                }).then(function (res) {
+                    if (res.data.fileType == ".jpg") {
+                        $scope.img = res.data.fileName;
+                    }
+                    else {
+                        $scope.pdf = res.data.fileName;
+                    }
+                })
+            }
+        };
     }
 
 })(angular.module('erp'));
